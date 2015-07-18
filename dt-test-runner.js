d7b9fe025be93e5c4a57f9d55e61c9cf0d3f9534
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2015-07-18 17:13:50 +0100

    Update dt-test-runner.js

--- dt-test-runner.js
+++ dt-test-runner.js
@@ -3,6 +3,10 @@ var glob = require('glob'),
     
 console.log('Is this a CI environment? %s', process.env.CI);
 console.log('Invoked dt-contribs test runner...');
+if(!process.env.CI) {
+    console.log('Please run in a CI environment such as Travis.');
+    process.exit(1);
+}
 glob('*/', function(err, modules) {
     for(var i = 0; i < modules.length; i++) {
 	process.chdir(modules[i]);

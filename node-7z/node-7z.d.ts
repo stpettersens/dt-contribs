7375adb8e0fbc675ee041013f7a78d031a8d6757
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2015-07-19 19:14:55 +0100

    +node-7z

--- node-7z/node-7z.d.ts
+++ node-7z/node-7z.d.ts
@@ -4,10 +4,10 @@
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
 declare module "node-7z" {
-	export function add(archive: string, files: string[], options?: Object): Object;
-	export function extract(archive: string, dest: string, options?: Object): Object;
-	export function extractFull(archive: string, dest: string, options?: Object): Object;
-	export function list(archive: string, options?: Object): Object;
-	export function test(archive: string, options?: Object): Object;
-	export function update(archive: string, files: string[], options?: Object): Object;
+    export function add(archive: string, files: string[], options?: Object): Object;
+    export function extract(archive: string, dest: string, options?: Object): Object;
+    export function extractFull(archive: string, dest: string, options?: Object): Object;
+    export function list(archive: string, options?: Object): Object;
+    export function test(archive: string, options?: Object): Object;
+    export function update(archive: string, files: string[], options?: Object): Object;
 }

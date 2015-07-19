459e69c35eaf1bfcbe7254908725ed320d361d7d
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2015-07-19 18:24:40 +0100

    Update glob-tests.ts

--- glob/glob-tests.ts
+++ glob/glob-tests.ts
@@ -2,10 +2,9 @@
 
 import glob = require('glob');
 
-glob('*.ts', {debug: false}, function(err: any, files: string[]) {
+glob.glob('*.ts', {debug: false}, function(err: any, files: string[]) {
 	console.log('glob is matching:');
 	for(var i: number = 0; i < files.length; i++) {
 		console.log(files[i]);
 	}
 });
-

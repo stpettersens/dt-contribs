a080e06bfae94f7b5ff5a53b324e3df4e6b50e75
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2015-07-19 18:59:45 +0100

    +magic-number

--- magic-number/magic-number-tests.ts
+++ magic-number/magic-number-tests.ts
@@ -1,13 +1,12 @@
+/// <reference path="../node/node.d.ts" />
 /// <reference path="magic-number.d.ts" />
 
 import fs = require('fs');
 import magic = require('magic-number');
 
-var buffer = new Buffer(100);
+var buffer: any = new Buffer(100);
 buffer.write('7z', 'binary');
-fs.writeFileSync('test.7z', buffer);
-var mimetype: string = magic.detectFile('test.7z');
-console.log(mimetype); // => 'application/7z-x-compressed'
-fs.unlinkSync('test.7z');
-
-
+fs.writeFile('test.love', buffer, function(err: any) {
+    console.log(magic.detectFile('test.love')); // => 'application/7z-x-compressed'
+    fs.unlinkSync('test.love');
+});

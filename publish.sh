92c744b59d0adca5f6063995dbf2e62c85191fde
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2015-07-21 11:12:49 +0100

    ...

--- publish.sh
+++ publish.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 # Publish a module here to my DefinitelyTyped fork.
+# Usage: sh publish module_name
+
 mkdir ../DefinitelyTyped/$1
 cp $1/$1.d.ts ../DefinitelyTyped/$1
 cp $1/$1-tests.ts ../DefinitelyTyped/$1
+cd ../DefinitelyTyped
+git add $1
 git commit -a -m "Type definitions and tests for $1"
 git status

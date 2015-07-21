12005ef9856b37848bde145c9ebbe523831b5a40
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2015-07-21 13:53:15 +0100

    fixed jquery-toolip?

--- jquery-tooltip/jquery-tooltip-tests.ts
+++ jquery-tooltip/jquery-tooltip-tests.ts
@@ -1,16 +1,5 @@
-/// <reference path="../node/node.d.ts" />
-/// <reference path="../jsdom/jsdom.d.ts" />
 /// <reference path="../jquery/jquery.d.ts" />
 /// <reference path="jquery-tooltip.d.ts" />
 
-import fs = require('fs');
-import jsdom = require('node-jsdom');
-import $ = require('jquery');
-$(jsdom.jsdom('<div id="tt"></div>'.parentWindow));
-
-fs.readFile('jquery-tooltip.min.js', 'utf-8', function(err: any, code: string) {
-	eval(code);
-	console.log($('#tt').tooltip());
-}); 
-
+$.tooltip();
 

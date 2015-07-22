9fe59c15c0852ebbeee22110c41bd1ebd09b2ea4
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2015-07-22 17:51:04 +0100

    Delete blah.d.ts

--- blah/blah.d.ts
+++
@@ -1,8 +0,0 @@
-// Type definitions for blah
-// Project: https://github.com/USERNAME/blah
-// Definitions by: Sam Saint-Pettersen <https://github.com/stpettersens>
-// Definitions: https://github.com/borisyankov/DefinitelyTyped
-
-declare module "blah" {
-    // Implementation here...
-}

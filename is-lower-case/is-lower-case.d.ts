ccdeefe8f34127680d4de69d949c5040d5635b51
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2015-07-28 17:16:44 +0100

    ...

--- is-lower-case/is-lower-case.d.ts
+++ is-lower-case/is-lower-case.d.ts
@@ -1,9 +1,9 @@
 // Type definitions for is-lower-case
-// Project: https://github.com/blakeembrey/is-lower-case 
-// Definitions by: Sam Saint-Pettersen <https://github.com/stpettersens> 
+// Project: https://github.com/blakeembrey/is-lower-case
+// Definitions by: Sam Saint-Pettersen <https://github.com/stpettersens>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
 declare module "is-lower-case" {
-	function isLowerCase(string: string): boolean;
+	function isLowerCase(string: string, locale?: string): boolean;
 	export = isLowerCase;
 }

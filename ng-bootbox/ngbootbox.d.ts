ec8db612dbdcc412c588a2f3d329848ee26b773e
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2015-11-06 16:19:09 +0000

    Update and rename ng-bootbox.d.ts to ngbootbox.d.ts

--- ng-bootbox/ngbootbox.d.ts
+++ ng-bootbox/ngbootbox.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for ng-bootbox
+// Type definitions for ngbootbox
 // Project: https://github.com/eriktufvesson/ngBootbox
 // Definitions by: Sam Saint-Pettersen <https://github.com/stpettersens>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
